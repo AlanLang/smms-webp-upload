8d3541cc7668303a0dbf858fd081cbdf4bad5fa0
alan <langwdalan@gmail.com>
2023-08-02 23:11:32 +0800

    update

--- app/index.tsx
+++ app/index.tsx
@@ -9,10 +9,17 @@ function App() {
 
   return (
     <div className="flex items-center justify-center flex-col h-full">
-      <div className="mx-auto w-[90%] h-[90%] grid  gap-3 grid-rows-[20vh_10vh]">
+      <div className="mx-auto w-[90%] h-[90%] grid  gap-4 grid-rows-[20vh_10vh]">
         <Upload
           onUpload={(fs) => {
-            const allFiles = files ? [...fs, ...files] : fs;
+            const allFiles = files
+              ? [
+                  ...fs.filter(
+                    (f) => !files.some((item) => item.name === f.name)
+                  ),
+                  ...files,
+                ]
+              : fs;
             setFiles(allFiles);
           }}
         />
